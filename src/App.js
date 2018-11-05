26a66621707ab1ec9d773bebedbe3808f50d79da
Yiannis Stavgianoudakis <stavgian@gmail.com>
2018-11-05 21:27:59 +0100

    Colors Graph

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Gameboard from './Gameboard/Gameboard';
+import { Graph } from './Graph';
+import Grid from './components/Grid/Grid';
+import ColorPickers from './components/ColorPickers/ColorPickers';
+
+const SIZE = 6;
+const COLORS = ['blue','red','green','yellow','orange'];
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.incrementCount = this.incrementCount.bind(this);
+    this.sliderInput = this.sliderInput.bind(this);
+    this.restart = this.restart.bind(this);
+    this.colorFill = this.colorFill.bind(this);
+    this.state = {
+      size: SIZE,
+      graph: new Graph(SIZE),
+      colors: COLORS,
+      count: 0,
+    }
+  }
+
+  incrementCount() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  restart() {
+    this.setState({
+      graph: new Graph(this.state.size),
+      count: 0
+    });
+  }
+
+  colorFill(color) {
+    this.state.graph.colorFill(color);
+  }
+
+
+  sliderInput(value) {
+    this.setState({
+      size: value,
+      graph: this.newGrid(value, this.state.colors),
+      count: 0
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Gameboard></Gameboard>
-        </header>
-       
-      </div>
+        <div className="content">
+          <div className="header">
+            <h1>Color Flood</h1>
+            <div className="newgame" onClick={(e) => this.restart()}>New Game</div>
+            <div className="count">Changes <span>{this.state.count}</span></div>
+          </div>
+            <div><ColorPickers colors={this.state.colors} clickHandler={this.colorFill} incrementCount={this.incrementCount}/></div>
+          <Grid grid={this.state.graph} colors={this.state.colors} size={this.state.size}/>
+        </div>
     );
   }
 }
 
-export default App;
+
+export default (App);
